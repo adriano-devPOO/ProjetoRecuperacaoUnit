6854d30f585c91d54eeae368f04799bec8ee5f60
adriano-devPOO <desenvolvedor.adriano@gmail.com>
2021-02-25 22:25:11 -0300

    Finalizando as classes de domínio

--- src/main/java/com/adriano/unit/recuperacao/dominio/Livro.java
+++ src/main/java/com/adriano/unit/recuperacao/dominio/Livro.java
@@ -1,12 +1,28 @@
 package com.adriano.unit.recuperacao.dominio;
 
-public class Livro {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Livro implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nomeAutor;
 	private String titulo;
 	private String texto;
 
+	@OneToMany
+	@JoinColumn(name = "categoriaId")
 	private Categoria categoria;
 
 	public Livro() {
