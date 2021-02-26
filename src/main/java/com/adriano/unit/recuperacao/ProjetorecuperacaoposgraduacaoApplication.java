e9b3d3a2cc4393581c6eebb898dbd50e74d84209
adriano-devPOO <desenvolvedor.adriano@gmail.com>
2021-02-25 21:40:42 -0300

    Commit inicial

---
+++ src/main/java/com/adriano/unit/recuperacao/ProjetorecuperacaoposgraduacaoApplication.java
@@ -0,0 +1,13 @@
+package com.adriano.unit.recuperacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetorecuperacaoposgraduacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetorecuperacaoposgraduacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adriano/unit/recuperacao/ProjetorecuperacaoposgraduacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriano.unit.recuperacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetorecuperacaoposgraduacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
