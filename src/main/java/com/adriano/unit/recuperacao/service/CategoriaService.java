898faf204b9ea15fda03bff2f251a953f9f4486e
adriano-devPOO <desenvolvedor.adriano@gmail.com>
2021-03-02 21:21:38 -0300

    Exceção personalizada para delete de categoria

--- src/main/java/com/adriano/unit/recuperacao/service/CategoriaService.java
+++ src/main/java/com/adriano/unit/recuperacao/service/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.adriano.unit.recuperacao.dominio.Categoria;
 import com.adriano.unit.recuperacao.dto.CategoriaDTO;
+import com.adriano.unit.recuperacao.exceptions.DataIntegratyViolationException;
 import com.adriano.unit.recuperacao.exceptions.ObjectNotFoundException;
 import com.adriano.unit.recuperacao.repositorios.CategoriaRepositorio;
 
@@ -23,12 +25,12 @@ public class CategoriaService {
 		return objeto.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repositorio.findAll();
 	}
-	
-	public Categoria create (Categoria objeto) {
+
+	public Categoria create(Categoria objeto) {
 		objeto.setId(null);
 		return repositorio.save(objeto);
 	}
@@ -42,7 +44,12 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		findById(id);
-		repositorio.deleteById(id);
-		
+
+		try {
+			repositorio.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegratyViolationException("Categoria não pode ser deletado! Possui livros associados.");
+		}
+
 	}
 }
