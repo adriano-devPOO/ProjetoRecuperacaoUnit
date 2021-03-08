4f988bbac905f33c6693f5426147b5268e2f444c
adriano-devPOO <desenvolvedor.adriano@gmail.com>
2021-03-08 11:54:11 -0300

    Inserir uma anotação, @CrossOrigin(*), nas classes livro e categoria resource

--- src/main/java/com/adriano/unit/recuperacao/resources/LivroResource.java
+++ src/main/java/com/adriano/unit/recuperacao/resources/LivroResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,6 +25,7 @@ import com.adriano.unit.recuperacao.dominio.Livro;
 import com.adriano.unit.recuperacao.dto.LivroDTO;
 import com.adriano.unit.recuperacao.service.LivroService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
